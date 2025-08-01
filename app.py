6a410c14a42f5e778df8fbd7bf6f71ab1bb31c8e
dopamiini <ttkorkki3@gmail.com>
2025-08-01 10:29:18 +0300

    Added basic functionality for requirements (registration, login, logout)

--- app.py
+++ app.py
@@ -1,7 +1,60 @@
 from flask import Flask
+from flask import abort, redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+import config
+import db
+import sqlite3
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
+
+def active_session():
+    if "username" not in session:
+        abort(403)
 
 @app.route("/")
 def index():
-    return "This is the template for the food recipe application!"
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "Error: the passwords did not match."
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "Error: username is already taken."
+
+    return "Account has been created."
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        sql = "SELECT password_hash FROM users WHERE username = ?"
+        password_hash = db.query(sql, [username])[0][0]
+
+        if check_password_hash(password_hash, password):
+            session["username"] = username
+            return redirect("/")
+        else:
+            return "Error: incorrect username or password."
+
+@app.route("/logout")
+def logout():
+    active_session()
+    del session["username"]
+    return redirect("/")
