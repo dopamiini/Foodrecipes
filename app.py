46cb8f59e7ae29b82da4a2e1d10770e7f7734425
dopamiini <ttkorkki3@gmail.com>
2025-08-01 12:58:51 +0300

    Added the ability to add recipes.

--- app.py
+++ app.py
@@ -16,6 +16,23 @@ def active_session():
 def index():
     return render_template("index.html")
 
+@app.route("/new_recipe")
+def new_recipe():
+    return render_template("new_recipe.html")
+
+@app.route("/create_recipe", methods=["POST"])
+def create_recipe():
+    title = request.form["title"]
+    description = request.form["description"]
+    ingredients = request.form["ingredients"]
+    instructions = request.form["instructions"]
+    user_id = session["user_id"]
+
+    sql = "INSERT INTO recipes (title, notes, ingredients, instructions, user_id) VALUES (?, ?, ?, ?, ?)"
+    db.execute(sql, [title, description, ingredients, instructions, user_id])
+    
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -44,10 +61,13 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -56,5 +76,6 @@ def login():
 @app.route("/logout")
 def logout():
     active_session()
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,12 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE recipes (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    notes TEXT,
+    ingredients TEXT,
+    instructions TEXT,
+    user_id INTEGER REFERENCES users
+);
